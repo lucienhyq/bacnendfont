e454abfdc61ba095f6a2ffa8783c2a5d1671b6e1
SwagDad <47840560+lucienhyq@users.noreply.github.com>
2023-04-04 18:16:07 +0800

    zancun

--- src/components/pcCnav.vue
+++ src/components/pcCnav.vue
@@ -1,43 +1,23 @@
 <template>
   <div class="pccNav">
     <div class="pccNavTab">
-      <el-col :span="24">
-        <h5>默认颜色</h5>
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-        >
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
+      <el-col>
+        <el-menu default-active="2" background-color="#545c64" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-menu-item index="1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航1</span>
+          </el-menu-item>
           <el-menu-item index="2">
             <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+            <span slot="title">导航2</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
+          <el-menu-item index="3">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航3</span>
           </el-menu-item>
           <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航4</span>
           </el-menu-item>
         </el-menu>
       </el-col>
@@ -61,10 +41,11 @@ export default {
 
 <style scoped lang="scss">
 .pccNav {
-  background: #929292;
+  background: #545c64;
+  overflow: hidden;
   .pccNavTab {
     height: 100%;
-    width: 10rem;
+    // width: 10rem;
   }
 }
 </style>
